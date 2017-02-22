636ccd7f448761dd2b2255d7170c67af31677f2f
Brennan Govreau <brennangovreau@yahoo.com>
2017-02-21 18:33:22 -0700

    Move logic for tables into new PlayerStatTable class

--- HockeyStats/PlayerStatForm.Designer.cs
+++ HockeyStats/PlayerStatForm.Designer.cs
@@ -28,50 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.mainTableDGV = new System.Windows.Forms.DataGridView();
             this.addPlayerButton = new System.Windows.Forms.Button();
             this.playerIdTextbox = new System.Windows.Forms.TextBox();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.secondaryTableDGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.mainTableDGV)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondaryTableDGV)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // mainTableDGV
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.LightGray;
-            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle5;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle6;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.Size = new System.Drawing.Size(967, 322);
-            this.dataGridView1.TabIndex = 0;
+            this.mainTableDGV.AllowUserToAddRows = false;
+            this.mainTableDGV.AllowUserToDeleteRows = false;
+            this.mainTableDGV.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.LightGray;
+            this.mainTableDGV.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.mainTableDGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.mainTableDGV.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.mainTableDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.mainTableDGV.DefaultCellStyle = dataGridViewCellStyle2;
+            this.mainTableDGV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTableDGV.Location = new System.Drawing.Point(0, 0);
+            this.mainTableDGV.Name = "mainTableDGV";
+            this.mainTableDGV.ReadOnly = true;
+            this.mainTableDGV.RowHeadersVisible = false;
+            this.mainTableDGV.Size = new System.Drawing.Size(967, 322);
+            this.mainTableDGV.TabIndex = 0;
             // 
             // addPlayerButton
             // 
@@ -98,7 +99,7 @@
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel1.Controls.Add(this.mainTableDGV);
             // 
             // splitContainer1.Panel2
             // 
@@ -113,7 +114,7 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.dataGridView2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.secondaryTableDGV, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -137,14 +138,23 @@
             this.tableLayoutPanel2.Size = new System.Drawing.Size(245, 26);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
-            // dataGridView2
+            // secondaryTableDGV
             // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView2.Location = new System.Drawing.Point(3, 35);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(961, 422);
-            this.dataGridView2.TabIndex = 1;
+            this.secondaryTableDGV.AllowUserToAddRows = false;
+            this.secondaryTableDGV.AllowUserToDeleteRows = false;
+            this.secondaryTableDGV.AllowUserToOrderColumns = true;
+            this.secondaryTableDGV.DefaultCellStyle = dataGridViewCellStyle2;
+            this.secondaryTableDGV.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.secondaryTableDGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.secondaryTableDGV.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.secondaryTableDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.secondaryTableDGV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.secondaryTableDGV.Location = new System.Drawing.Point(3, 35);
+            this.secondaryTableDGV.Name = "secondaryTableDGV";
+            this.secondaryTableDGV.ReadOnly = true;
+            this.secondaryTableDGV.RowHeadersVisible = false;
+            this.secondaryTableDGV.Size = new System.Drawing.Size(961, 422);
+            this.secondaryTableDGV.TabIndex = 1;
             // 
             // PlayerStatForm
             // 
@@ -154,7 +164,7 @@
             this.Controls.Add(this.splitContainer1);
             this.Name = "PlayerStatForm";
             this.Text = "Hockey Stats";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainTableDGV)).EndInit();
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
@@ -162,20 +172,20 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondaryTableDGV)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView mainTableDGV;
         private System.Windows.Forms.Button addPlayerButton;
         private System.Windows.Forms.TextBox playerIdTextbox;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView secondaryTableDGV;
     }
 }
 
---
+++ HockeyStats/PlayerStatTable.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace HockeyStats
+{
+    public class PlayerStatTable
+    {
+        DataTable dataTable;
+        private DataGridView dataGridView;
+        private List<string> playerIds;
+        private List<string> columnData;
+        private List<string> displayYears;
+
+        public PlayerStatTable(DataGridView dataGridView, List<string> playerIds, List<string> columnData, List<string> displayYears)
+        {
+            this.dataGridView = dataGridView;
+            this.playerIds = playerIds;
+            this.columnData = columnData;
+            this.displayYears = displayYears;
+
+            dataTable = CreateDataTable(this.dataGridView);
+            new Thread(() => FillDataTable()).Start(); // Fill the main table in a separate thread
+        }
+
+        public void AddPlayerToDataTable(string playerId)
+        {
+            Dictionary<string, string> playerDict = new Dictionary<string, string>();
+            AddPlayerStatsToDict(playerDict, playerId);
+            AddDraftDataToDict(playerDict, playerId);
+            AddRowToDataTable(playerDict);
+        }
+
+        private DataTable CreateDataTable(DataGridView dgv)
+        {
+            DataTable dataTable = new DataTable();
+            foreach (string columnName in columnData)
+            {
+                dataTable.Columns.Add(new DataColumn(columnName));
+            }
+            dgv.DataSource = dataTable;
+            return dataTable;
+        }
+
+        private void FillDataTable()
+        {
+            // Get Elite Prospects data for each player
+            foreach (string playerId in playerIds)
+            {
+                AddPlayerToDataTable(playerId);
+            }
+        }
+
+        private void AddPlayerStatsToDict(Dictionary<string, string> playerDict, string playerId)
+        {
+            JObject statsJson = EliteProspectsAPI.GetPlayerStats(playerId);
+            foreach (JToken statLine in statsJson["data"])
+            {
+                StatLineParser stats = new StatLineParser(statLine);
+                if (stats.GetGameType() == "REGULAR_SEASON" && (displayYears == null || displayYears.Count == 0 || displayYears.Contains(stats.GetSeasonName())))
+                {
+                    if (playerDict.Count == 0)
+                    {
+                        playerDict["First Name"] = stats.GetFirstName();
+                        playerDict["Last Name"] = stats.GetLastName();
+                        playerDict["Games Played"] = stats.GetGamesPlayed();
+                        playerDict["Goals"] = stats.GetGoals();
+                        playerDict["Assists"] = stats.GetAssists();
+                        playerDict["Total Points"] = stats.GetTotalPoints();
+                        playerDict["PPG"] = stats.GetPointsPerGame();
+                        playerDict["League"] = stats.GetLeagueName();
+                        playerDict["Team"] = stats.GetTeamName();
+                    }
+                    else
+                    {
+                        playerDict["Games Played"] += Environment.NewLine + stats.GetGamesPlayed();
+                        playerDict["Goals"] += Environment.NewLine + stats.GetGoals();
+                        playerDict["Assists"] += Environment.NewLine + stats.GetAssists();
+                        playerDict["Total Points"] += Environment.NewLine + stats.GetTotalPoints();
+                        playerDict["PPG"] += Environment.NewLine + stats.GetPointsPerGame();
+                        playerDict["League"] += Environment.NewLine + stats.GetLeagueName();
+                        playerDict["Team"] += Environment.NewLine + stats.GetTeamName();
+                    }
+                }
+            }
+        }
+
+        private void AddDraftDataToDict(Dictionary<string, string> playerDict, string playerId)
+        {
+            JObject draftJson = EliteProspectsAPI.GetPlayerDraftData(playerId);
+            JToken data = draftJson["data"];
+            if (data != null)
+            {
+                DraftDataParser draftData = new DraftDataParser(data.First);
+                playerDict["Draft Year"] = draftData.GetYear();
+                playerDict["Draft Round"] = draftData.GetRound();
+                playerDict["Draft Overall"] = draftData.GetOverall();
+                playerDict["Draft Team"] = draftData.GetTeamName();
+            }
+        }
+
+        private void AddRowToDataTable(Dictionary<string, string> rowDict)
+        {
+            string[] orderedRowValues = new string[columnData.Count];
+            for (int i = 0; i < columnData.Count; i++)
+            {
+                string outString = String.Empty;
+                // If the column exists in the row, add the value
+                if (rowDict.TryGetValue(columnData[i], out outString))
+                {
+                    orderedRowValues[i] = rowDict[columnData[i]];
+                }
+            }
+            dataTable.Rows.Add(orderedRowValues);
+        }
+    }
+}
